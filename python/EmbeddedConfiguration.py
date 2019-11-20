c9880187836e24d573506d896c9270fc468698af
Andrew Loeliger <aloelige@cern.ch>
2019-11-20 09:08:04 -0500

    Update 206 tt to use embedded

--- python/EmbeddedConfiguration.py
+++ python/EmbeddedConfiguration.py
@@ -1,6 +1,6 @@
 #configuration file for determining which distributions we will run the model for embedded on
 EmbeddedConfiguration = {
-    "2016tt":False,
+    "2016tt":True,
     "2016mt":True,
     "2016et":True,
     "2016em":True,
