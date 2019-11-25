0046b6ccc18d468530f057b3717a9072471ddbf9
Sam <samhiggie72@gmail.com>
2019-11-25 10:08:54 -0600

    KappaVKappaF interface and plotting scripts added

--- scripts/RunCombineFits.py
+++ scripts/RunCombineFits.py
@@ -35,6 +35,9 @@ parser.add_argument('--SplitSTXS',help="Split STXS measurements into component p
 parser.add_argument('--RunParallel',help='Run all fits in parallel using threads',action="store_true")
 parser.add_argument('--numthreads',nargs='?',help='Number of threads to use to run fits in parallel',type=int,default=12)
 parser.add_argument('--DecorrelateForMe',help="Run the decorrelator as part of the overall run. Looks for a datacard named smh<year><channel>_nocorrelation.root",action="store_true")
+parser.add_argument('--StoreShapes', help = "Store pre and post-fit shapes for use later",action = "store_true")
+parser.add_argument('--RunKappaVKappaF',help="Runs kappa_V and kappa_F scan",action="store_true")
+parser.add_argument('--Asimov',help="Use the Asimov dataset in the limit calculation - only available for kappa_V and kappa_F scan at the moment",action="store_true")
 print("Parsing command line arguments.")
 args = parser.parse_args() 
 
@@ -88,7 +91,7 @@ for year in args.years:
             DataCardCreationCommand+=" -q"
         DataCardCreationCommand+=" --Categories"
         for Category in cfg.Categories[channel]:
-            DataCardCreationCommand+=" "+cfg.Categories[channel][Category]
+            DataCardCreationCommand+=" "+Category
         print("Creating data cards")
         logging.info("Data Card Creation Command:")
         logging.info('\n\n'+DataCardCreationCommand+'\n')
@@ -108,7 +111,7 @@ for year in args.years:
         CardNum = 1
         TheFile = ROOT.TFile(os.environ['CMSSW_BASE']+"/src/auxiliaries/shapes/smh"+year+channel+".root")
         for Directory in TheFile.GetListOfKeys():
-            if Directory.GetName() in cfg.Categories[channel].values():
+            if Directory.GetName() in cfg.Categories[channel]:
                 if not args.RunWithoutAutoMCStats:
                     CardFile = open(OutputDir+"smh"+year+"_"+channel+"_"+str(CardNum)+"_13TeV_.txt","a+")
                     CardFile.write("* autoMCStats 0.0\n")
@@ -235,7 +238,10 @@ ExtraCombineOptions = '--robustFit=1 --preFitValue=1. --X-rtd FITTER_NEW_CROSSIN
 if args.ComputeSignificance:
     PhysModel = 'Significance'
     ExtraCombineOptions = '--X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --cl=0.68'
-
+if args.StoreShapes:
+    PhysModel = 'FitDiagnostics'
+    ExtraCombineOptions = '--robustFit=1 --preFitValue=1. --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP  --cl=0.68 --saveShapes'
+    
 #run the inclusive
 CombinedWorkspaceName = CombinedCardName[:len(CombinedCardName)-3]+"root"
 InclusiveCommand="combineTool.py -M "+PhysModel+" "+CombinedWorkspaceName+" "+ExtraCombineOptions+" --expectSignal=1 -t -1"
@@ -361,7 +367,7 @@ if args.ComputeGOF:
           CardNum = 1
           TheFile = ROOT.TFile(os.environ['CMSSW_BASE']+"/src/auxiliaries/shapes/smh"+year+channel+".root")
           for Directory in TheFile.GetListOfKeys():
-              if Directory.GetName() in cfg.Categories[channel].values():
+              if Directory.GetName() in cfg.Categories[channel]:
                  ImpactCommand = "text2workspace.py -m 125 smh"+year+"_"+channel+"_"+str(CardNum)+"_13TeV_.txt "
                  os.system(ImpactCommand)
                  GOFJsonName = "gof_"+channel+"_"+year+"_"+str(CardNum)+"_"+DateTag+".json"
@@ -380,7 +386,39 @@ if args.ComputeGOF:
                  CardNum+=1
 
     os.chdir("../../")
+if (args.RunKappaVKappaF and args.Asimov):
+    os.chdir(OutputDir)
+
+    KappaVKappaFcmd = "text2workspace.py -m 125 -P HiggsAnalysis.CombinedLimit.HiggsCouplings:cVcF --PO BRU=0 "+OutputDir+"FinalCard_"+DateTag+".txt"+"-o comb_htt_kvkf.root"
+    os.system(KappaVKappaFcmd)
+
+    KappaVKappaFcmd = "combine -M MultiDimFit -m 125 -n htt -t -1000 --setParameterRanges kappa_V=0.0,5.0:kappa_F=0.0,5.0 comb_htt_kvkf.root --algo=singles --robustFit=1" # get the central point!  
+    os.system(KappaVKappaFcmd)
+    
+    KappaVKappaFcmd = "combine -n KvKfgrid_tt -M MultiDimFit -m 125 -t -1000 --setParameterRanges kappa_V=0.0,5.0:kappa_F=0.0,2.0 comb_htt_kvkf.root --algo=grid --points=1000 --parallel=8"   #  now dance around the central point - change the points for granularity
+    os.system(KappaVKappaFcmd)
+
+    KappaVKappaFcmd = "python plotKVKF.py -o plot_kVkF -f tau=higgsCombineKvKfgrid_tt.MultiDimFit.mH125.root --order=\"tau\" --legend-order=\"tau\" --layout 1 --x-range 0.0,5.0 --y-range 0.0,3.0 --axis-hist 200,0.0,5.0,200,0.0,3.0"
+    os.system(KappaVKappaFcmd)
+
+if (args.RunKappaVKappaF and not args.Asimov):
+    os.chdir(OutputDir)
+
+    KappaVKappaFcmd = "text2workspace.py -m 125 -P HiggsAnalysis.CombinedLimit.HiggsCouplings:cVcF --PO BRU=0 "+OutputDir+"FinalCard_"+DateTag+".txt"+"-o comb_htt_kvkf.root"
+    os.system(KappaVKappaFcmd)
+
+    KappaVKappaFcmd = "combine -M MultiDimFit -m 125 -n htt --setParameterRanges kappa_V=0.0,5.0:kappa_F=0.0,5.0 comb_htt_kvkf.root --algo=singles --robustFit=1" # get the central point!  
+    os.system(KappaVKappaFcmd)
+    
+    KappaVKappaFcmd = "combine -n KvKfgrid_tt -M MultiDimFit -m 125 --setParameterRanges kappa_V=0.0,5.0:kappa_F=0.0,2.0 comb_htt_kvkf.root --algo=grid --points=1000 --parallel=8"   #  now dance around the central point
+    os.system(KappaVKappaFcmd)
+
+    KappaVKappaFcmd = "python plotKVKF.py -o plot_kVkF -f tau=higgsCombineKvKfgrid_tt.MultiDimFit.mH125.root --order=\"tau\" --legend-order=\"tau\" --layout 1 --x-range 0.0,5.0 --y-range 0.0,3.0 --axis-hist 200,0.0,5.0,200,0.0,3.0"
+    os.system(KappaVKappaFcmd)
 
 if args.RunParallel:
     ThreadHandler.BeginFits()
     ThreadHandler.WaitForAllThreadsToFinish()
+
+if args.StoreShapes:
+    os.system('mv '+os.environ['CMSSW_BASE']+"/src/CombineHarvester/Run2HTT_Combine/fitDiagnostics.Test.root "+OutputDir)
