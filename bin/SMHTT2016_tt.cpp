45ce9d6c07cbed4858cf04b04c93d22274ad503c
Andrew Loeliger <aloelige@cern.ch>
2019-12-11 07:23:20 -0500

    removed slight 2016 bug

--- bin/SMHTT2016_tt.cpp
+++ bin/SMHTT2016_tt.cpp
@@ -155,7 +155,7 @@ int main(int argc, char **argv)
 
       // Prefiring
       AddShapesIfNotEmpty({"CMS_prefiring"},
-                          JoinStr({sig_procs,{"VVL","STL","ZL","TTL","TTT"}}),
+                          JoinStr({sig_procs,{"VVL","STL","ZL","TTL"}}),
                           &cb,
                           1.00,
                           TheFile,CategoryArgs);
