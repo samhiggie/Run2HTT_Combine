9967f14cbc8406ec67678fdfee826dda710e34ca
Andrew Loeliger <aloelige@cern.ch>
2019-12-13 12:00:58 -0500

    change the explicit H tau tau couplings to capture STXS bins

--- scripts/RunCombineFits.py
+++ scripts/RunCombineFits.py
@@ -130,8 +130,8 @@ os.system(CardCombiningCommand)
 print("Setting up per signal workspace")
 PerSignalName = OutputDir+"Workspace_per_signal_breakdown_cmb_"+DateTag+".root"
 PerSignalWorkspaceCommand = "text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel "
-PerSignalWorkspaceCommand+= "--PO 'map=.*/ggH_htt125.*:r_ggH[1,-25,25]' "
-PerSignalWorkspaceCommand+= "--PO 'map=.*/qqH_htt125.*:r_qqH[1,-25,25]' "
+PerSignalWorkspaceCommand+= "--PO 'map=.*/ggH.*htt125.*:r_ggH[1,-25,25]' "
+PerSignalWorkspaceCommand+= "--PO 'map=.*/qqH.*htt125.*:r_qqH[1,-25,25]' "
 PerSignalWorkspaceCommand+= "--PO 'map=.*/WH_htt125.*:r_WH[1,-25,25]' "
 PerSignalWorkspaceCommand+= "--PO 'map=.*/ZH_htt125.*:r_ZH[1,-25,25]' "
 PerSignalWorkspaceCommand+= CombinedCardName +" -o "+PerSignalName+" -m 125"
