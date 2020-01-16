d029068b697738cebc9463ced01cbb52f73c4d64
Andrew Loeliger <aloelige@cern.ch>
2020-01-16 08:32:05 -0500

    Changed the way blinding works, and upped the size of slice labels.

--- python/PlottingModules/prefitPostfitSettings/blinding.py
+++ python/PlottingModules/prefitPostfitSettings/blinding.py
@@ -1,56 +1,33 @@
 import ROOT
 import math
-
-#clculate the background content at given point
-#takes the point number, and the full histogram dictionary
-def CalculateB(i,FullDictionary):
-    try:
-        content = FullDictionary['jetFakes'].GetBinContent(i)
-    except KeyError:
-        content = FullDictionary['QCD'].GetBinContent(i)
-        content += FullDictionary['W'].GetBinContent(i)
-    content += FullDictionary['ZT'].GetBinContent(i)
-    content += FullDictionary['ZL'].GetBinContent(i)
-    content += FullDictionary['TTL'].GetBinContent(i)
-    try:
-        content += FullDictionary['TTT'].GetBinContent(i)
-    except KeyError:
-        pass
-    content += FullDictionary['VVL'].GetBinContent(i)
-    try:
-        content += FullDictionary['VVT'].GetBinContent(i)
-    except KeyError:
-        pass
-    content += FullDictionary['STL'].GetBinContent(i)
-    try:
-        content += FullDictionary['STT'].GetBinContent(i)
-    except KeyError:
-        pass
-    return content
+from CombineHarvester.Run2HTT_Combine.PlottingModules.prefitPostfitSettings.rebinning import GetNSlices
 
 #this function will blind our datapoints,
-#i.e set them to -1 upon a given condition
-#the current condition is just S/root(B) > 0.5
-def BlindDataPoints(SignalDictionary,FullDictionary,DataDictionary):
-    dataPointRangeLow = 1
-    dataPointRangeHigh = DataDictionary['data_obs'].GetNbinsX() + 1
-    for i in range(dataPointRangeLow,dataPointRangeHigh):
-        backgroundContentAtPoint = CalculateB(i,FullDictionary)
-        signalContentAtPoint = SignalDictionary['Higgs'].GetBinContent(i)        
-        try:
-            if signalContentAtPoint / math.sqrt(backgroundContentAtPoint) > 0.5:
-                DataDictionary['data_obs'].SetBinContent(i,0.0)
-        except ZeroDivisionError:
-            print("Skipping zero prediction bin...")
+#current blinding scheme is to just blind mtt range 90-150
+#this corresponds to bins 3,4 and 5, in each slices.
+def BlindDataPoints(DataDictionary,category):
+    nSlices = GetNSlices(category)    
+    for i in range(nSlices):
+        #backgroundContentAtPoint = CalculateB(i,FullDictionary)
+        #signalContentAtPoint = SignalDictionary['Higgs'].GetBinContent(i)        
+        #try:
+        #    if signalContentAtPoint / math.sqrt(backgroundContentAtPoint) > 0.5:
+        bin_3 = 3+i*9
+        bin_4 = 4+i*9
+        bin_5 = 5+i*9
+        DataDictionary['data_obs'].SetBinContent(bin_3,0.0)
+        DataDictionary['data_obs'].SetBinContent(bin_4,0.0)
+        DataDictionary['data_obs'].SetBinContent(bin_5,0.0)
+        #except ZeroDivisionError:
+        #    print("Skipping zero prediction bin...")        
 
-def BlindRatioPlot(SignalDictionary,FullDictionary,ratioPlot):
-    ratioRangeLow = 1
-    ratioRangeHigh = ratioPlot.GetN()
-    for i in range(ratioRangeLow,ratioRangeHigh):
-        backgroundContentAtPoint = CalculateB(i,FullDictionary)
-        signalContentAtPoint = SignalDictionary['Higgs'].GetBinContent(i)
-        try:
-            if signalContentAtPoint / math.sqrt(backgroundContentAtPoint) > 0.5:
-                ratioPlot.SetPoint(i,-1,-1)
-        except ZeroDivisionError:
-            print("Skipping zero prediction bin...")
+def BlindRatioPlot(ratioPlot,category):
+    nSlices = GetNSlices(category)    
+    for i in range(nSlices):        
+        bin_3 = 3+i*9
+        bin_4 = 4+i*9
+        bin_5 = 5+i*9
+        ratioPlot.SetPoint(bin_3,-1,-1)
+        ratioPlot.SetPoint(bin_4,-1,-1)
+        ratioPlot.SetPoint(bin_5,-1,-1)
+        
