e79f1e9067fcf150d69b1e4aae2fb8c5c5755004
Andrew Loeliger <aloelige@cern.ch>
2020-01-29 08:44:24 -0500

    Rework the remove negative bins script to work

--- scripts/RemoveNegativeBins.py
+++ scripts/RemoveNegativeBins.py
@@ -11,17 +11,31 @@ if __name__ == "__main__":
     TheFile = ROOT.TFile(args.FileIn,"UPDATE")
 
     for i in range(TheFile.GetListOfKeys().GetEntries()):
-        TheDirectory = TheFile.Get(TheFile.GetListOfKeys().At(i).GetName())
-        for j in range(TheDirectory.GetListOfKeys().GetEntries()):
-            if re.search("(Up|Down)",TheDirectory.GetListOfKeys().At(j).GetName()):
-                HasNegativeBins=False
-                TheHisto = TheDirectory.Get(TheDirectory.GetListOfKeys().At(j).GetName()).Clone()
-                for k in range(1,TheHisto.GetNbinsX()):
-                    if TheHisto.GetBinContent(k) < 0.0:
-                        HasNegativeBins=True
-                        TheHisto.SetBinContent(k,0.0)
-                if HasNegativeBins:
-                    TheDirectory.cd()
-                    TheHisto.Write()
-                        
-                
+        TheDirectory = TheFile.Get(TheFile.GetListOfKeys().At(i).GetName())                                   
+        #print(TheDirectory.GetName())
+        #print(TheDirectory.GetListOfKeys().GetEntries())
+        #print(range(10))
+        #print(range(TheDirectory.GetListOfKeys().GetEntries()))
+        #for j in range(TheDirectory.GetListOfKeys().GetEntries()):            
+        numEntries = TheDirectory.GetListOfKeys().GetEntries()
+        #for j in range(40):            
+        j=0
+        while j < numEntries:
+            #print(j)
+            HasNegativeBins=False            
+            #TheHisto = TheDirectory.Get(TheDirectory.GetListOfKeys().At(j).GetName()).Clone()
+            TheHisto = TheDirectory.Get(TheDirectory.GetListOfKeys().At(j).GetName())
+            #print(TheDirectory.GetListOfKeys().At(j).GetName())
+            #raw_input()
+            for k in range(1,TheHisto.GetNbinsX()):
+                if TheHisto.GetBinContent(k) < 0.0:
+                    HasNegativeBins=True
+                    TheHisto.SetBinContent(k,0.0)
+            if HasNegativeBins:     
+                j+=2
+                numEntries+=1
+                #print("!")
+                TheDirectory.cd()
+                TheHisto.Write()                                
+            else:
+                j+=1
